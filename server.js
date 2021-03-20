34ceca54f5df06755d0dbb7dd862743e60afb427
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-03-20 10:37:51 -0500

    added auth helper function

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
-//const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
 require('dotenv').config();
 
-//const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);

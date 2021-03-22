0fcda3bd4d33f99416ebfe2fd4502f1b1982bbd2
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-03-21 19:04:51 -0500

    add ability to create post through FE

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const path = require('path');
 
 require('dotenv').config();
 
-//const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
